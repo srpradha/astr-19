bfe404cee6bdfdf2da13d7258043b9e9d861f3a5
srpradha <115218336+srpradha@users.noreply.github.com>
2023-09-29 16:14:55 -0700

    Session 1 prompt for Coding Journal 1.

---
+++ cj1-1.py
@@ -0,0 +1,34 @@
+"""What I learned:
+I learned to format strings and assign strings to variables. I also learned how to print strings (with colors!).
+"""
+
+# constants for ANSI color codes
+GREEN = "\033[92m"
+MAGENTA = "\033[35m"
+CYAN = "\033[36m"
+RESET = "\033[0m"
+
+
+# main function
+def main():
+    # variables
+    full_name = "Samik Pradhan"
+    pronouns = "he/him/his"
+    fav_movie = "Interstellar"
+    fav_food = "ramen"
+
+    # full message
+    message = (
+        f"Name: {GREEN}{full_name}{RESET}\n"
+        + f"Pronouns: {MAGENTA}{pronouns}{RESET}\n"
+        + f"My favorite movie is {CYAN}{fav_movie}{RESET}.\n"
+        + f"My favorite food is {CYAN}{fav_food}{RESET}."
+    )
+
+    # print the message
+    print(message)
+
+
+# run the main function
+if __name__ == "__main__":
+    main()
